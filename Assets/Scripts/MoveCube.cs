69e83e49745c3ae387f971a58f0aa004cfd06ad2
nightm4re94 <matthias@gurkenlabs.de>
2020-05-17 17:20:55 +0200

    Initial Commit

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MoveCube : MoveCubeBehavior
+{
+    private void Update()
+    {
+        if (!networkObject.IsServer)
+        {
+            transform.position = networkObject.position;
+            return;
+        }
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * 5f;
+        networkObject.position = transform.position;
+    }
+}
